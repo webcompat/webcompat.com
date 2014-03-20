8e86b665c1715e7bd1ae3c204571a68cf946472b
Mike Taylor <miket@mozilla.com>
2014-03-20 12:35:56 -0500

    Reorganize things to be a bit more declarative.

--- webcompat/__init__.py
+++ webcompat/__init__.py
@@ -4,9 +4,13 @@
 
 from flask import Flask, render_template
 from flask.ext.github import GitHub
+from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+engine = create_engine('sqlite:///' + os.path.join(app.config['BASE_DIR'],
+                                                   'github-session.db'))
 github = GitHub(app)
 
 # import views after we initialize our github object
