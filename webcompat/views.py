48f6e27c6eddf2d600c371507e5b12efc7e26631
Mike Taylor <miket@mozilla.com>
2014-03-21 15:23:44 -0500

    Add our initial routes for the first launch.

--- webcompat/views.py
+++ webcompat/views.py
@@ -27,9 +27,60 @@ def after_request(response):
     return response
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user.github_access_token
+
+
+@app.route('/')
+@app.route('/index')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    if session.get('user_id', None) is None:
+        return github.authorize('public_repo')
+    else:
+        return 'Already logged in'
+
+
+# OAuth2 callback handler that GitHub requires.
+# If this moves, it needs to change in GitHub settings as well
+@app.route('/callback')
+@github.authorized_handler
+def authorized(access_token):
+    if access_token is None:
+        # TODO: not this, flash a message and send back?
+        return 'OH CRAP'
+    user = User.query.filter_by(github_access_token=access_token).first()
+    if user is None:
+        user = User(access_token)
+        db_session.add(user)
+    user.github_access_token = access_token
+    db_session.commit()
+
+    session['user_id'] = user.id
+    return redirect(url_for('new_issue'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/issues')
+def show_issues():
+    return redirect(url_for('new_issue'), code=307)
+
+
+#TODO: /issues/new/<issue> redirect 307 to github repo issue
+@app.route('/issues/new', methods=['GET', 'POST'])
+def new_issue():
     if request.method == 'POST' and g.user:
         # um, validation. probably should use wtfform?
         if request.form['title'] is not None:
@@ -42,51 +93,18 @@ def index():
     elif request.method == 'GET':
         if g.user:
             user_info = github.get('user')
-            return render_template('index.html',
+            return render_template('new_issue.html',
                                    person=user_info.get('login'),
                                    gravatar=user_info.get('gravatar_id'))
         else:
-            t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
+            t = ('To report an issue please login! '
+                 '<a href="{{ url_for("login") }}">Login</a>')
             return render_template_string(t)
 
 
-@github.access_token_getter
-def token_getter():
-    user = g.user
-    if user is not None:
-        return user.github_access_token
-
-
-# This is the OAuth2 callback handler that Github requires.
-@app.route('/callback')
-@github.authorized_handler
-def authorized(access_token):
-    if access_token is None:
-        return 'OH CRAP'
-
-    user = User.query.filter_by(github_access_token=access_token).first()
-    if user is None:
-        user = User(access_token)
-        db_session.add(user)
-    user.github_access_token = access_token
-    db_session.commit()
-
-    session['user_id'] = user.id
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    if session.get('user_id', None) is None:
-        return github.authorize('public_repo')
-    else:
-        return 'Already logged in'
-
-
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('index'))
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @app.errorhandler(404)
