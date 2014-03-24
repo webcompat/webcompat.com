d991dda013db08fb81a263b2777fcdc728ca4881
Mike Taylor <miket@mozilla.com>
2014-03-24 12:00:01 -0500

    Do last modified date via python (creating a custom template filter).

---
+++ webcompat/template_filters.py
@@ -0,0 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from webcompat import app
+
+@app.template_filter('format_date')
+def last_modified_format(value):
+    return value.strftime('%B %d, %Y.')
