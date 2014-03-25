f2223fcd149cc61a522e1e3808531d455ae0cca9
Mike Taylor <miket@mozilla.com>
2014-03-25 16:58:55 -0500

    Getting our form into a usable state:
    form validation working (needs some style love)

--- webcompat/issue_form.py
+++ webcompat/issue_form.py
@@ -2,22 +2,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from wtforms import (Form, BooleanField, RadioField, StringField,
-                     TextAreaField, validators)
+from wtforms import Form, RadioField, StringField, TextAreaField
+from wtforms.validators import Optional, Required
 
 
 owner_choices = [('site_owner_yes', 'Yes'), ('site_owner_no', 'No')]
 problem_choices = [('browser_bug', 'Looks like the browser has a bug'),
                    ('site_bug', 'Looks like the website has a bug.'),
                    ('dunno', 'I don\'t know but something\'s wrong.')]
+url_message = 'A valid URL is required to report a bug!'
+summary_message = 'Please give the bug report a summary.'
+
 
 class IssueForm(Form):
-    url = StringField('Site URL', [validators.InputRequired(),
-                                   validators.URL()])
-    site_owner = RadioField('Is this your website?', choices=owner_choices)
+    url = StringField('Site URL', [Required(message=url_message)])
+    browser = StringField('Browser', [Optional()])
+    version = StringField('Version', [Optional()])
+    summary = StringField('Problem in 5 words',
+                          [Required(message=summary_message)])
+    description = TextAreaField('How can we replicate this?', [Optional()])
+    site_owner = RadioField('Is this your website?', [Optional()],
+                            choices=owner_choices)
     problem_category = RadioField('What seems to be the trouble?',
-                                   choices=problem_choices)
-    browser = StringField('Browser')
-    version = StringField('Version')
-    summary = StringField('Problem in 5 words', [validators.InputRequired()])
-    description = TextAreaField('How can we replicate this?')
+                                  [Optional()], choices=problem_choices)
