701d3f1285a08c1a6f85ece83d451ae2cd45e0f7
Mike Taylor <miket@mozilla.com>
2014-04-16 13:22:18 -0500

    Inherit from unittest.TestCase (and use its methods)

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -5,53 +5,57 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-import unittest
+from unittest import TestCase
 import webcompat
 
+# Any request that depends on parsing HTTP Headers (basically anything
+# on the index route, will need to include the following: environ_base=headers
 headers = {'HTTP_USER_AGENT': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; '
-                                'rv:31.0) Gecko/20100101 Firefox/31.0')}
+                               'rv:31.0) Gecko/20100101 Firefox/31.0')}
 
 
-class TestURLs:
-    def setup(self):
+class TestURLs(TestCase):
+    def setUp(self):
         webcompat.app.config['TESTING'] = True
         self.app = webcompat.app.test_client()
 
-    def teardown(self):
+    def tearDown(self):
         pass
 
     def test_home(self):
         '''Test that the home page exists'''
         rv = self.app.get('/', environ_base=headers)
-        assert rv.status_code == 200
+        self.assertEqual(rv.status_code, 200)
 
     def test_about(self):
         '''Test that /about exists'''
         rv = self.app.get('/about')
-        assert rv.status_code == 200
+        self.assertEqual(rv.status_code, 200)
 
     def test_thanks(self):
         '''Test that /thanks exists'''
         rv = self.app.get('/thanks')
-        assert rv.status_code == 200
+        self.assertEqual(rv.status_code, 200)
 
     def test_login(self):
-        '''Test that the login route 302s to GitHub'''
+        '''Test that the /login route 302s to GitHub'''
         rv = self.app.get('/login')
-        assert rv.status_code == 302
-        assert 'github.com/login/oauth/' in rv.headers['Location']
+        self.assertEqual(rv.status_code, 302)
+        self.assertIn('github.com/login/oauth/', rv.headers['Location'])
 
     def test_issue_redirect(self):
-        '''Test that the login route 307s to GitHub issues (for now)'''
+        '''Test that the /issues/<number> route 307s to GitHub issues
+         (for now)'''
         rv = self.app.get('/issues/3')
-        assert rv.status_code == 307
-        assert 'nobody-look-at-this/issues/' in rv.headers['Location']
+        self.assertEqual(rv.status_code, 307)
+        self.assertRegexpMatches(rv.headers['Location'],
+                                 r'https.+github.+issues')
 
     def test_issues_redirect(self):
-        '''Test that the login route 307s to /index (for now)'''
+        '''Test that the /issues route 307s to /index (for now)'''
         rv = self.app.get('/issues')
-        assert rv.status_code == 307
-        assert 'localhost' in rv.headers['Location']
+        self.assertEqual(rv.status_code, 307)
+        self.assertIn('localhost', rv.headers['Location'])
 
 
 if __name__ == '__main__':
