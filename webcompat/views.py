2a078c2371e90663ca167414eadce9aa8e5aa921
Mike Taylor <miket@mozilla.com>
2014-04-30 21:30:29 -0500

    Add cache-busting to static JS and CSS assets
    Based on when the app was started, i.e., redeployed.

--- webcompat/views.py
+++ webcompat/views.py
@@ -7,15 +7,22 @@
 import os
 import time
 import urllib
-from datetime import datetime
 from flask import (flash, g, redirect, request, render_template, session,
                    url_for, abort)
 from flask.ext.github import GitHubError
 from form import (build_formdata, get_browser_name, get_browser_version,
                   IssueForm, AUTH_REPORT, PROXY_REPORT)
+from hashlib import md5
 from issues import report_issue, proxy_report_issue
 from models import db_session, User
 from webcompat import github, app
+
+
+@app.context_processor
+def cache_buster():
+    def bust_cache():
+        return md5(app.config['STARTUP']).hexdigest()[:14]
+    return dict(bust_cache=bust_cache)
 
 
 @app.teardown_appcontext
