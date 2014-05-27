e4fe0b77820853b6bacd4dbb300450cd596eab81
Mike Taylor <miket@mozilla.com>
2014-05-27 13:34:41 -0500

    Add all changes related to Ember on the homepage.
    Mostly to create a record, I'm planning on backing a lot of this out.

---
+++ webcompat/static/js/lib/app.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+window.App = Ember.Application.create({
+  rootElement: '#browse-issues',
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    var ownerURI = 'https://api.github.com/repos/webcompat/web-bugs/issues?creator=' + window.__username;
+    return Ember.RSVP.hash({
+      myIssues: Ember.$.getJSON(ownerURI),
+      //Only show 4 results for the non-mine issues
+      needsDiagnosis: Ember.$.getJSON('https://api.github.com/repos/webcompat/web-bugs/issues').then(function(data) {
+        return data.filter(function(issue){
+          if (issue.labels.indexOf("contactready") == -1) {
+            return true;
+          }
+        }).slice(0,4);
+      }),
+      contactReady: Ember.$.getJSON('https://api.github.com/repos/webcompat/web-bugs/issues?labels=contactready').then(function(data) {
+         return data.slice(0,4)
+      })
+    });
+  }
+});
