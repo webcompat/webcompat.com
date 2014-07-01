2938e09e1000668cb8b2f92e8843a86cf877616e
Mike Taylor <miket@mozilla.com>
2014-07-01 14:03:01 -0500

    Change /thanks/ page to point to internal pages.

--- webcompat/views.py
+++ webcompat/views.py
@@ -183,8 +183,7 @@ def show_issue(number):
 def thanks(number):
     if number.isdigit():
         issue = number
-        uri = u"https://github.com/{0}/{1}".format(
-            app.config['ISSUES_REPO_URI'], number)
+        uri = u"http://webcompat.com/issues/{0}".format(number)
         text = u"I just filed a bug on the internet: "
         encoded_issue = urllib.quote(uri.encode("utf-8"))
         encoded_text = urllib.quote(text.encode("utf-8"))
@@ -192,8 +191,7 @@ def thanks(number):
         abort(404)
     return render_template('thanks.html', number=issue,
                            encoded_issue=encoded_issue,
-                           encoded_text=encoded_text,
-                           repo_uri=app.config['ISSUES_REPO_URI'])
+                           encoded_text=encoded_text)
 
 
 @app.route('/about')
