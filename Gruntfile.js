9a5252ab0a1a1309e606d3be2bb9c9319a017fe2
Mike Taylor <miket@mozilla.com>
2014-07-07 11:19:51 -0500

    Add marked.js for clientside markdown parsing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,8 +20,9 @@ module.exports = function(grunt) {
             '<%= jsPath %>/vendor/jquery-1.11.0.min.js',
             '<%= jsPath %>/vendor/underscore-min.js',
             '<%= jsPath %>/vendor/backbone-min.js',
-            '<%= jsPath %>/lib/homepage.js',
             '<%= jsPath %>/vendor/moment-min.js',
+            '<%= jsPath %>/vendor/marked-min.js',
+            '<%= jsPath %>/lib/homepage.js',
             '<%= jsPath %>/lib/bugform.js'
         ],
         dest: '<%= jsPath %>/<%= pkg.name %>.js'
