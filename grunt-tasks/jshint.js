22d1b1212e177b85797abfe1f94cb0acc262aa64
magsout <g.demesy@splitfire.fr>
2014-07-08 14:04:07 +0200

    remove Gruntfile.js

---
+++ grunt-tasks/jshint.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.config('jshint', {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true,
+          $: true,
+          _: true,
+          Backbone: true,
+          console: true,
+          issueNumber: true,
+          marked: true,
+          moment: true
+        }
+      },
+      beforeconcat: [
+        '<%= jsPath %>/lib/homepage.js',
+        '<%= jsPath %>/lib/bugform.js',
+        '<%= jsPath %>/lib/issues.js'
+      ]
+  });	
+
+};
