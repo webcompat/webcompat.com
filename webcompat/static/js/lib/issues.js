836a3e999ebf985610e53bc01511e7a255392661
Mike Taylor <miket@mozilla.com>
2014-07-28 17:25:26 -0500

    Rudimentary nuts and bolts for editing labels.

--- webcompat/static/js/lib/issues.js
+++ webcompat/static/js/lib/issues.js
@@ -50,6 +50,34 @@ issues.Issue = Backbone.Model.extend({
       reporter: response.user.login,
       title: response.title
     });
+  },
+  updateLabels: function(labelsArray) {
+    var self = this;
+    if (!$.isArray(labelsArray)) {
+      return;
+    }
+
+    $.ajax({
+      contentType: 'application/json',
+      data: JSON.stringify(labelsArray),
+      type: 'POST',
+      url: '/api/issues/' + this.get('number') + '/labels',
+      success: function(response) {
+        //update model after success
+        self.set('labels', JSON.parse(response));
+      },
+      error: function() {
+        $('<div></div>', {
+          'class': 'flash error',
+          'text': 'There was an error setting labels.'
+        }).appendTo('body');
+
+        setTimeout(function(){
+          var __flashmsg = $('.flash');
+          if (__flashmsg.length) {__flashmsg.fadeOut();}
+        }, 2000);
+      }
+    });
   }
 });
 
@@ -88,6 +116,16 @@ issues.CommentView = Backbone.View.extend({
   }
 });
 
+// Read-only Model of all labels in the repo
+issues.AllLabels = Backbone.Model.extend({
+  url: function() {
+    return '/api/issues/labels';
+  },
+  parse: function(response) {
+    this.set({labels: response});
+  }
+});
+
 issues.TitleView = Backbone.View.extend({
   el: $('.issue__main_title'),
   template: _.template($('#title-tmpl').html()),
@@ -115,6 +153,89 @@ issues.BodyView = Backbone.View.extend({
   }
 });
 
+issues.LabelsView = Backbone.View.extend({
+  el: $('.issue__label'),
+  editorButton: null,
+  events: {
+    'click .issue__label--modify:not(.is-disabled)': 'editLabels'
+  },
+  template: _.template($('#issue-labels-tmpl').html()),
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    this.fetchLabels();
+    return this;
+  },
+  renderLabels: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+  },
+  fetchLabels: function() {
+    var self = this;
+    var headersBag = {headers: {'Accept': 'application/json'}};
+    this.editorButton = $('.issue__label--modify');
+    this.allLabels = new issues.AllLabels();
+    this.labelEditor = new issues.LabelEditorView({
+      model: this.allLabels,
+      issueView: this,
+    });
+    this.allLabels.fetch(headersBag).success(function(){
+      self.issueLabels = _.pluck(self.model.get('labels'), 'name');
+      self.repoLabels = _.pluck(self.labelEditor.model.get('labels'), 'name');
+      self.editorButton.show();
+    });
+  },
+  editLabels: function() {
+    this.editorButton.addClass('is-disabled');
+    this.$el.after(this.labelEditor.render().el);
+    var toBeChecked = _.intersection(this.issueLabels, this.repoLabels);
+    _.each(toBeChecked, function(labelName) {
+      $('[name=' + labelName + ']').prop("checked", true);
+    });
+  }
+});
+
+issues.LabelEditorView = Backbone.View.extend({
+  className: 'issue__label_editor',
+  events: {
+    'change input[type=checkbox]': 'updateView',
+    'click button': 'closeEditor'
+  },
+  initialize: function(options) {
+    this.issueView = options.issueView;
+  },
+  template: _.template($('#label-editor-tmpl').html()),
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+  reRender: function(data) {
+    this.issueView.$el.html(this.issueView.template(data));
+    this.issueView.$el.find('.issue__label--modify').addClass('is-disabled');
+  },
+  updateView: function() {
+    // we do the "real" save when you close the editor.
+    // this just updates the UI responsively
+    var checked = $('input[type=checkbox]:checked');
+    // build up an array of objects that have
+    // .name and .color props that the templates expect
+    var modelUpdate = [];
+    _.each(checked, function(item) {
+      //item already has a .name property
+      item.color = item.dataset.color;
+      modelUpdate.push(item);
+    });
+    this.reRender({labels: modelUpdate});
+  },
+  closeEditor: function() {
+    var checked = $('input[type=checkbox]:checked');
+    var labelsArray = _.pluck(checked, 'name');
+    this.issueView.editorButton.removeClass('is-disabled');
+    this.issueView.model.updateLabels(labelsArray);
+    // detach() (vs remove()) here because we don't want to lose events if the
+    // user reopens the editor.
+    this.$el.children().detach();
+  }
+});
+
 issues.MainView = Backbone.View.extend({
   el: $('.issue'),
   events: {
@@ -132,17 +253,20 @@ issues.MainView = Backbone.View.extend({
     this.title = new issues.TitleView({model: this.issue});
     this.metadata = new issues.MetaDataView({model: this.issue});
     this.body = new issues.BodyView({model: this.issue});
+    this.labels = new issues.LabelsView({model: this.issue});
   },
   fetchModels: function() {
     var self = this;
     var headersBag = {headers: {'Accept': 'application/json'}};
     this.issue.fetch(headersBag).success(function() {
-      _.each([self.title, self.metadata, self.body, self], function(elm) {
-        elm.render();
-        _.each($('.issue__details code'), function(elm){
-          Prism.highlightElement(elm);
-        });
-      });
+      _.each([self.title, self.metadata, self.body, self.labels, self],
+        function(elm) {
+          elm.render();
+          _.each($('.issue__details code'), function(elm) {
+            Prism.highlightElement(elm);
+          });
+        }
+      );
 
       // If there are any comments, go fetch the model data
       if (self.issue.get('commentNumber') > 0) {
