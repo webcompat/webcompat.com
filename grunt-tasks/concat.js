bee7e7c403f545416330e79fa37226ce8598918f
Mike Taylor <miket@mozilla.com>
2014-08-01 22:16:39 -0500

    Remove underscore, update grunt, etc.

--- grunt-tasks/concat.js
+++ grunt-tasks/concat.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
       dist: {
         src: [
             '<%= jsPath %>/vendor/jquery-1.11.0.min.js',
-            '<%= jsPath %>/vendor/underscore-min.js',
+            '<%= jsPath %>/vendor/lodash.underscore-min.js',
             '<%= jsPath %>/vendor/backbone-min.js',
             '<%= jsPath %>/vendor/moment-min.js',
             '<%= jsPath %>/vendor/prism.js',
