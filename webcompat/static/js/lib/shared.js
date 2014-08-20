1387056e08e96ccb5b21023d18f52fb9f36c8e01
magsout <guillaume.demesy@gmail.com>
2014-08-20 15:55:45 +0200

    move function

--- webcompat/static/js/lib/shared.js
+++ webcompat/static/js/lib/shared.js
@@ -9,3 +9,18 @@
     logLink.prop('href', href + '?next=' + location.href);
   });
 }());
+
+function removeQuotes(string) {
+    if (typeof string === 'string' || string instanceof String) {
+        string = string.replace(/^['"]+|\s+|\\|(;\s?})+|['"]$/g, '');
+    }
+    return string;
+}
+function getBreakpoint() {
+    var style;
+    if ( window.getComputedStyle && window.getComputedStyle(document.body, '::after') ) {
+        style = window.getComputedStyle(document.body, '::after');
+        style = style.content;
+    }
+    return JSON.parse( removeQuotes(style) );
+}
