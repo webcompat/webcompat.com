04d78068d80bf5e7a3f22008a5f8e8afb30b50e1
Mike Taylor <miket@mozilla.com>
2014-09-08 17:31:20 -0500

    Functional tests for contributors interaction.

---
+++ tests/functional/lib/contributors.js
@@ -0,0 +1,75 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define([
+  'intern',
+  'intern!object',
+  'intern/chai!assert',
+  'require'
+], function (intern, registerSuite, assert, require) {
+  'use strict';
+
+  var url = intern.config.siteRoot + '/contributors';
+
+  registerSuite({
+    name: 'contributors',
+
+    'page loads': function () {
+      return this.remote
+        .get(require.toUrl(url))
+        .findByCssSelector('.contributors__main_title').getVisibleText()
+        .then(function (text) {
+          assert.include(text, 'Join the Webcompat team!');
+        })
+        .end();
+    },
+
+    'clicking section toggles it': function() {
+      return this.remote
+        .get(require.toUrl(url))
+        .findByCssSelector('.contributors__item__title').click()
+        .end()
+        .findByCssSelector('.contributors__item__content.is-open').isDisplayed()
+        .then(function (isDisplayed) {
+          assert.equal(isDisplayed, true);
+        })
+        .end()
+        .findByCssSelector('.contributors__light.is-active').isDisplayed()
+        .then(function (isDisplayed) {
+          assert.equal(isDisplayed, true);
+        })
+        .end()
+        .findByCssSelector('.contributors__item__title').click()
+        .end()
+        .findByCssSelector('.contributors__item__content').getAttribute('class')
+        .then(function (className) {
+          assert.notInclude('is-open', className);
+        })
+        .end()
+        .findByCssSelector('.contributors__light').getAttribute('class')
+        .then(function (className) {
+          assert.notInclude('is-active', className);
+        })
+    },
+
+    'toggling section toggles lightbulb': function() {
+      return this.remote
+        .get(require.toUrl(url))
+        .findByCssSelector('.contributors__item__title').click()
+        .end()
+        .findByCssSelector('.contributors__light.is-active').isDisplayed()
+        .then(function (isDisplayed) {
+          assert.equal(isDisplayed, true);
+        })
+        .end()
+        .findByCssSelector('.contributors__item__title').click()
+        .end()
+        .findByCssSelector('.contributors__light').getAttribute('class')
+        .then(function (className) {
+          assert.notInclude('is-active', className);
+        })
+    },
+
+  });
+});
