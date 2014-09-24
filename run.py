38adadee942e4b57bd5b640e50f4379f1ce36fb8
Karl Dubost <karl+github@la-grange.net>
2014-09-24 16:00:50 +0900

    Adding feature for checking the BOT_OAUTH_TOKEN #284

--- run.py
+++ run.py
@@ -5,10 +5,29 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import argparse
+import sys
 
 from webcompat import app
 
+BOT_HELP = '''
+The BOT_OAUTH_TOKEN is not configured in your config file.
+You will need to set up one on github for testing your
+local developments.
+Read Instructions at
+https://github.com/webcompat/webcompat.com/blob/master/CONTRIBUTING.md#configuring-the-server
+'''
+
+
+def config_validator():
+    '''Make sure the config file is ready.'''
+    # Checking if there is a bot configured
+    if app.config['BOT_OAUTH_TOKEN'] == '':
+        sys.exit(BOT_HELP)
+
 if __name__ == '__main__':
+    # testing the config file
+    config_validator()
+    # Parsing arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--testmode', action='store_true',
                         help='Run server in "test mode".')
