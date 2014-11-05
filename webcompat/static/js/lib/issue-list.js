ab4f7bda5dd7760eb7943a3910cb58609065dbd7
Mike Taylor <miket@mozilla.com>
2014-11-05 13:07:44 -0600

    Closes #329 - only display search for logged in users.

--- webcompat/static/js/lib/issue-list.js
+++ webcompat/static/js/lib/issue-list.js
@@ -343,7 +343,10 @@ issueList.MainView = Backbone.View.extend({
     this.issueSorter = new issueList.SortingView();
     this.filter = new issueList.FilterView();
     this.paginationControls = new issueList.PaginationControlsView();
-    this.search = new issueList.SearchView();
+    // only init the SearchView if the user is logged in.
+    if (this.isLoggedIn) {
+      this.search = new issueList.SearchView();
+    }
 
     this.render();
   },
@@ -352,9 +355,13 @@ issueList.MainView = Backbone.View.extend({
     this.$el.fadeIn(_.bind(function() {
       this.filter.render();
       this.issueSorter.render();
-      this.search.render();
+
+      if (this.isLoggedIn) {
+        this.search.render();
+      }
     }, this));
-  }
+  },
+  isLoggedIn: $('body').data('username')
 });
 
 //Not using a router, so kick off things manually
