a348731c175649d1ae6b541b87c64559e905d827
Mike Taylor <miket@mozilla.com>
2014-12-18 13:43:14 -0600

    Issue #270 - Test that form is hidden or shown depending on auth state.

--- tests/functional/comments.js
+++ tests/functional/comments.js
@@ -6,8 +6,9 @@ define([
   'intern',
   'intern!object',
   'intern/chai!assert',
-  'require'
-], function (intern, registerSuite, assert, require) {
+  'require',
+  'tests/functional/lib/helpers'
+], function (intern, registerSuite, assert, require, FunctionalHelpers) {
   'use strict';
 
   var url = function(num) {
@@ -21,18 +22,23 @@ define([
       return this.remote
         .setFindTimeout(intern.config.wc.pageLoadTimeout)
         .get(require.toUrl(url(100)))
-        .sleep(1000)
         .findByCssSelector('.Comment--form').isDisplayed()
         .then(function (isDisplayed) {
           assert.equal(isDisplayed, true, 'Comment form visible for logged in users.');
         })
         .end()
+    },
+
+    'Comment form not visible for logged out users': function() {
+      return this.remote
+        .setFindTimeout(intern.config.wc.pageLoadTimeout)
+        .get(require.toUrl(url(100)))
+        // logout
         .findByCssSelector('.wc-Navbar-section--right .wc-Navbar-link').click()
         .end()
-        .findAllByCssSelector('.Comment--form').isDisplayed()
-        .then(function (isDisplayed) {
-          assert.equal(isDisplayed, false, 'Comment form hidden visible for logged out users.');
-        });
+        // make sure the comment form isn't on the page
+        FunctionalHelpers.noElementByCssSelector(this, '.Comment--form')
+        .end();
     }
 
   });
