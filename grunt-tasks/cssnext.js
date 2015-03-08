ea544d8089763589e0832b8f9afe628cf61464fe
Guillaume Demesy <guillaume.demesy@gmail.com>
2015-03-08 22:24:14 +0100

    Updated cssnext.j - better to not rebased url

--- grunt-tasks/cssnext.js
+++ grunt-tasks/cssnext.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   grunt.config('cssnext', {
     options: {
       sourcemap: true,
+      url : false
       browsers: ['ff >= 4', 'ie >= 8', 'safari >= 5.1', 'opera >= 12', 'chrome >=10'],
       import: { path: ["node_modules"] }
     },
