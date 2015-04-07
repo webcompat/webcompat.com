a3cd00e4aab4a1d64122e26a74a78f01acaf5779
Mike Taylor <miket@mozilla.com>
2015-04-07 13:39:26 -0500

    Whitespace fix.

--- grunt-tasks/imagemin.js
+++ grunt-tasks/imagemin.js
@@ -1,29 +1,26 @@
 module.exports = function(grunt) {
   grunt.config('imagemin', {
-		png: {
-			options: {
-            	optimizationLevel: 7
-			},
-			files: [
-			{
-				expand				: true,
-				cwd					: '<%= imgPath %>/',
-				src					: ['*.png'],
-				dest				: '<%= imgPath %>/'
-			}]
-		},
-        jpg: {
-            options: {
-                progressive: true
-            },
-            files: [
-                {
-				expand				: true,
-				cwd					: '<%= imgPath %>/',
-				src					: ['*.jpg'],
-				dest				: '<%= imgPath %>/'
-                }
-            ]
-        }
-	});
+    png: {
+      options: {
+        optimizationLevel: 7
+      },
+      files: [{
+        expand: true,
+        cwd:    '<%= imgPath %>/',
+        src:    ['*.png'],
+        dest:   '<%= imgPath %>/'
+      }]
+    },
+    jpg: {
+      options: {
+        progressive: true
+      },
+      files: [{
+        expand: true,
+        cwd:    '<%= imgPath %>/',
+        src:    ['*.jpg'],
+        dest:   '<%= imgPath %>/'
+        }]
+      }
+  });
 };
