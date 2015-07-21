5e2ae26c225653fd2ade32f9b05e8d69a8d7b1b0
Karl Dubost <karl+github@la-grange.net>
2015-07-21 16:25:25 +0900

    adding a first test for labels

---
+++ tests/functional/labels.js
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define([
+  'intern',
+  'intern!object',
+  'intern/chai!assert',
+  'require'
+], function (intern, registerSuite, assert, require) {
+  'use strict';
+
+  var url = function (num) {
+    return intern.config.siteRoot + '/issues/' + num;
+  };
+
+  registerSuite({
+    name: 'labels',
+
+    'setting a label': function () {
+      return this.remote
+        .setFindTimeout(intern.config.wc.pageLoadTimeout)
+        .get(require.toUrl(url(100)))
+        .sleep(1000)
+        .findByCssSelector('LabelEditor-launcher').click()
+        .sleep(2000)
+        .end()
+        .findByCssSelector('label.LabelEditor-item input[@name="contactready"]').click()
+        .sleep(2000)
+        .end()
+        .findByCssSelector('span.Label')
+        .then(function (text) {
+          assert.include(text, 'contactready', 'Label contactready has been set');
+        })
+        .end();
+    }
+  });
+});
