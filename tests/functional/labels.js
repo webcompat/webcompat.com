2bd745627cca8d5023d653b651c866ae9bc7006d
Karl Dubost <karl+github@la-grange.net>
2015-08-14 14:47:32 +0900

    #272 label sent to github

--- tests/functional/labels.js
+++ tests/functional/labels.js
@@ -70,6 +70,25 @@ define([
             assert.include(label_text, 'contactready', 'Label appears once it has been selected');
           })
           .end();
+      },
+
+      'Label has been sent to GitHub': function () {
+        return this.remote
+          .setFindTimeout(intern.config.wc.pageLoadTimeout)
+          .get(require.toUrl(url(2)))
+          .findByCssSelector('.LabelEditor-launcher').click()
+          .end()
+          .findByCssSelector('label.LabelEditor-item input[name="contactready"]').click()
+          .end()
+          .findByCssSelector('.LabelEditor-btn').click()
+          .end()
+          .refresh()
+          .findByCssSelector('.Label--badge')
+          .getVisibleText()
+          .then(function (label_text) {
+            assert.include(label_text, 'contactready', 'Label has been set on Github');
+          })
+          .end();
       }
     };
   });
