c8dc773cad47b0f41eba4ee2b60c7aad81e2d481
Mike Taylor <miket@mozilla.com>
2015-08-20 10:57:06 -0500

    Issue #24: Import TestingFileStorage (oops)

--- tests/test_uploads.py
+++ tests/test_uploads.py
@@ -11,6 +11,7 @@ import sys
 import unittest
 
 from flask import Request
+from flaskext.uploads import TestingFileStorage
 from StringIO import StringIO
 from werkzeug import FileStorage
 from werkzeug.datastructures import MultiDict
@@ -95,49 +96,6 @@ class TestUploads(unittest.TestCase):
                     file=(StringIO('Fake image data'), filename),
                 ))
             self.assertEqual(rv.status_code, status_code)
-
-
-class TestingFileStorage(FileStorage):
-    """
-    This is a helper for testing upload behavior in your application. You
-    can manually create it, and its save method is overloaded to set `saved`
-    to the name of the file it was saved to. All of these parameters are
-    optional, so only bother setting the ones relevant to your application.
-
-    This was copied from Flask-Uploads.
-
-    :param stream: A stream. The default is an empty stream.
-    :param filename: The filename uploaded from the client. The default is the
-                     stream's name.
-    :param name: The name of the form field it was loaded from. The default is
-                 ``None``.
-    :param content_type: The content type it was uploaded as. The default is
-                         ``application/octet-stream``.
-    :param content_length: How long it is. The default is -1.
-    :param headers: Multipart headers as a `werkzeug.Headers`. The default is
-                    ``None``.
-    """
-    def __init__(self, stream=None, filename=None, name=None,
-                 content_type='application/octet-stream', content_length=-1,
-                 headers=None):
-        FileStorage.__init__(
-            self, stream, filename, name=name,
-            content_type=content_type, content_length=content_length,
-            headers=None)
-        self.saved = None
-
-    def save(self, dst, buffer_size=16384):
-        """
-        This marks the file as saved by setting the `saved` attribute to the
-        name of the file it was saved to.
-
-        :param dst: The file to save to.
-        :param buffer_size: Ignored.
-        """
-        if isinstance(dst, basestring):
-            self.saved = dst
-        else:
-            self.saved = dst.name
 
 if __name__ == '__main__':
     unittest.main()
