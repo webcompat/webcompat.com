4a4e9a31869bbf35845cdde6f1c13df69ef3aa99
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-10-22 22:53:51 +0200

    #783 The new label model and related adjustments in label code

--- webcompat/static/js/lib/labels.js
+++ webcompat/static/js/lib/labels.js
@@ -4,30 +4,118 @@
 
  var issues = issues || {};
 
-// Read-only Model of all labels in the repo
-issues.AllLabels = Backbone.Model.extend({
+/**
+* A LabelList is a list of labels.
+*
+* It takes care of all namespace prefixing and unprefixing, so that
+* the rest of the app doesn't ever need to worry about those details.
+* To initialize, either pass in a list of labels as an array of strings
+* or an array of objects:
+*
+* new issues.LabelList({labels: ['firefox', 'ie', 'chrome']});
+*
+* new issues.LabelList({labels: [{name:'status-worksforme', url:'...',
+*    color:'cccccc'}]});
+*
+* Or a URL to a JSON file describing the labels:
+*
+* new issues.LabelList({url:'/path/to/labels.json'});
+*/
+
+issues.LabelList = Backbone.Model.extend({
+  initialize: function() {
+    this.set('namespaceRegex', /(browser|closed|os|status)-(.+)/i);
+    this.set('defaultLabelURL', '/api/issues/labels');
+    // The templating engine needs objects that have JS properties, it won't call
+    // get('labels'). Setting a property here makes sure we can pass the model
+    // directly to a template() method
+    this.on('change:labels', function(){
+      this.labels = this.get('labels');
+    });
+    // if we're initialized with {labels:array-of-objects}, process the data
+    var inputLabelData = this.get('labels');
+    this.set('labels', []);
+    if(inputLabelData) {
+      this.parse(inputLabelData);
+    } else {
+      // No input data, let's fetch it from a URL
+      if(!this.get('url')) {
+        // default to "all labels" URL
+        this.set('url', this.get('defaultLabelURL'));
+      }
+      var headersBag = {headers: {'Accept': 'application/json'}};
+      this.fetch(headersBag); // This will trigger parse() on response
+    }
+  },
+  parse: function(labelsArray){
+    var list = [];
+    var namespaceMap = {};
+    for(var i = 0, matches, theLabel; i < labelsArray.length; i++){
+      // We assume we either have an object with .name or an array of strings
+      var theLabel = labelsArray[i].name || labelsArray[i];
+      matches = theLabel.match(this.get('namespaceRegex'));
+      if(matches) {
+        namespaceMap[matches[2]] = matches[1];
+        list[i] = {
+          'name': matches[2],
+          'url': labelsArray[i].url,
+          'color': labelsArray[i].color,
+          'remoteName': matches[0]
+         };
+      }else {
+        if(typeof theLabel === 'object') {
+          list[i] = labelsArray[i];
+          list[i].remoteName = list[i].name;
+        } else {
+          list[i] = {'name': theLabel}
+        }
+      }
+    };
+    this.set('labels', list);
+    this.set('namespaceMap', namespaceMap);
+  },
+  // toPrefixed takes a local label name and maps it
+  // to the prefixed repository form. Also handles an array
+  // of label names (Note: not arrays of objects)
+  toPrefixed: function (input) {
+    if (typeof input === 'string') {
+      if(issues.allLabels.get('namespaceMap')[input]) {
+        return issues.allLabels.get('namespaceMap')[input] + '-' + input;
+      }
+      return input;
+    } else {
+      // This is not a string, we assume it's an array
+      return input.map(function(label){
+        return issues.allLabels.toPrefixed(label);
+      });
+    }
+  },
   url: function() {
-    return '/api/issues/labels';
+    return this.get('url');
   },
-  // See also issues.Issue#removeNamespaces
-  removeNamespaces: function(labelsArray) {
-    // Return a copy of labelsArray with the namespaces removed.
-    var namespaceRegex = /(browser|closed|os|status)-/i;
-    var labelsCopy = _.cloneDeep(labelsArray);
-    return _.map(labelsCopy, function(labelObject) {
-      labelObject.name = labelObject.name.replace(namespaceRegex, '');
-      return labelObject;
-    });
+  // Returns a simple array of unprefixed labels - strings only
+  toArray: function(){
+    return _.pluck(this.get('labels'), 'name');
   },
-  parse: function(response) {
-    this.set({
-      // Store a copy of the original response, so we can reconstruct
-      // the labels before talking back to the API.
-      namespacedLabels: response,
-      labels: this.removeNamespaces(response)
-    });
+  // To save the model to the server, we need to make
+  // sure we apply the prefixes the server expects.
+  // The JSON serialization will take care of it.
+  toJSON: function(){
+    var labelsArray = _.pluck(this.get('labels'), 'name');
+    return issues.allLabels.toPrefixed(labelsArray);
   }
 });
+
+// We need a complete list of labels for certain operations,
+// especially namespace mapping. If the list we're handling
+// doesn't happen to contain all the labels initially, it
+// can't get prefixing/unprefixing right when labels in previously
+// unseen namespaces are added in their local name form.
+// Hence, we set up a single, globally accessible "all labels" model
+// This is set up as early as possible to avoid timing issues
+if(!issues.allLabels) {
+  issues.allLabels = new issues.LabelList();
+}
 
 issues.LabelsView = Backbone.View.extend({
   _isLoggedIn: $('body').data('username'),
@@ -63,19 +151,13 @@ issues.LabelsView = Backbone.View.extend({
   fetchLabels: function() {
     var headersBag = {headers: {'Accept': 'application/json'}};
     this.editorButton = $('.LabelEditor-launcher');
-    this.allLabels = new issues.AllLabels();
     this.labelEditor = new issues.LabelEditorView({
-      model: this.allLabels,
+      model: issues.allLabels,
       issueView: this,
     });
-    // Stash the allLabels model so we can get it from Issue model later
-    this.model.set('repoLabels', this.allLabels);
     if (this._isLoggedIn) {
-      this.allLabels.fetch(headersBag).success(_.bind(function(){
-        this.issueLabels = this.getIssueLabels();
-        this.repoLabels = _.pluck(this.labelEditor.model.get('labels'), 'name');
-        this.editorButton.show();
-      }, this));
+      this.issueLabels = this.getIssueLabels();
+      this.editorButton.show();
     }
   },
   getIssueLabels: function() {
@@ -84,7 +166,7 @@ issues.LabelsView = Backbone.View.extend({
   editLabels: function() {
     this.editorButton.addClass('is-active');
     this.$el.find('.LabelEditor-launcher').after(this.labelEditor.render().el);
-    var toBeChecked = _.intersection(this.getIssueLabels(), this.repoLabels);
+    var toBeChecked = _.intersection(this.getIssueLabels(), issues.allLabels.toArray());
     _.each(toBeChecked, function(labelName) {
       $('[name=' + labelName + ']').prop('checked', true);
     });
@@ -106,7 +188,7 @@ issues.LabelEditorView = Backbone.View.extend({
   },
   template: _.template($('#label-editor-tmpl').html()),
   render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
+    this.$el.html(this.template(this.model));
     this.resizeEditorHeight();
     _.defer(_.bind(function() {
       this.$el.find('.LabelEditor-search').focus();
@@ -169,16 +251,15 @@ issues.LabelEditorView = Backbone.View.extend({
       return s.replace(/[-\/\\^$*+?:.()|[\]{}]/g, '\\$&');
     };
     var re = new RegExp('^' + escape(e.target.value), 'i');
-    var matches = _.pluck(_.filter(this.model.get('labels'), function(label) {
-      return re.test(label.name);
-    }), 'name');
+    var toHide = _.filter(this.model.toArray(), function(label) {
+      return !re.test(label);
+    });
 
     // make sure everything is showing
     $('.LabelEditor-item').show();
 
     // hide the non-filter matches
-    var hidden = _.difference(_.pluck(this.model.get('labels'), 'name'), matches);
-    _.each(hidden, function(name) {
+    _.each(toHide, function(name) {
       $('input[name=' + escape(name) + ']').closest('.LabelEditor-item').hide();
     });
   }, 100)
