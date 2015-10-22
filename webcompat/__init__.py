8d57fc28ce587e6be159cd0c09462f9f77e0ced9
Guillaume Demesy <guillaume.demesy@gmail.com>
2015-10-22 08:40:55 +0200

    Issue #749 - export Categories list in right place

--- webcompat/__init__.py
+++ webcompat/__init__.py
@@ -9,7 +9,6 @@
 import logging
 import os
 
-from collections import namedtuple
 from flask.ext.cache import Cache
 from flask.ext.github import GitHub
 from flask.ext.limiter import Limiter
@@ -39,19 +38,3 @@ file_handler = logging.FileHandler(app.config['LOG_FILE'])
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(logging.Formatter(app.config['LOG_FMT']))
 app.logger.addHandler(file_handler)
-
-# Status categories used in the project
-# 'new', 'needsdiagnosis', 'needscontact', 'contactready' , 'sitewait', 'close'
-# Creating the model
-Category = namedtuple('Category', ['name', 'dataAttribute', 'label'])
-categories = []
-cat_labels = [('new', 'new', 'New Issues'),
-              ('needsDiagnosis', 'needsdiagnosis', 'Needs Diagnosis'),
-              ('needsContact', 'needscontact', 'Needs Contact'),
-              ('sitewait', 'sitewait', 'Site Contacted'),
-              ('close', 'closed', 'Closed')]
-# populating the categories for issues status
-for cat_label in cat_labels:
-    categories.append(Category(name=cat_label[0],
-                               dataAttribute=cat_label[1],
-                               label=cat_label[2]))
