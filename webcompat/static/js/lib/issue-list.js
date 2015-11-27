be071373e3f3a517aeb725cd4fc5e81f80f7a49c
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-11-27 01:32:58 +0100

    Refactored search code

--- webcompat/static/js/lib/issue-list.js
+++ webcompat/static/js/lib/issue-list.js
@@ -14,11 +14,13 @@ if (!window.md) {
 }
 
 issueList.DropdownView = Backbone.View.extend({
+  mainView: null,
   events: {
     'click .js-dropdown-toggle': 'openDropdown',
     'click .js-dropdown-options li': 'selectDropdownOption'
   },
-  initialize: function() {
+  initialize: function(options) {
+    this.mainView = options.mainView;
     // this.el is set from parent view via setElement
     // handles closing dropdown when clicking "outside".
     $(document).on('click', _.bind(function(e) {
@@ -54,10 +56,8 @@ issueList.DropdownView = Backbone.View.extend({
     } else if (typeof e.type === 'string') {
       // we're dealing with a user click event.
       option = $(e.target);
-      params = option.data('params');
-
-      // fire an event so other views can react to dropdown changes
-      wcEvents.trigger('dropdown:change', params, {update: true});
+      params = option.data('params').split(/=/);
+      this.mainView.params.setParam(params[0], params[1]);
       this.updateDropdownTitle(option);
       e.preventDefault();
     }
@@ -84,12 +84,14 @@ issueList.DropdownView = Backbone.View.extend({
 
 issueList.FilterView = Backbone.View.extend({
   el: $('.js-issuelist-filter'),
+  mainView: null,
   events: {
     'click .js-filter-button': 'toggleFilter'
   },
   _isLoggedIn: $('body').data('username'),
   _userName: $('body').data('username'),
-  initialize: function() {
+  initialize: function(theOptions) {
+    this.mainView = theOptions.mainView;
     //TODO: move this model out into its own file once we have
     //actual data for issues count
     issueList.events.on('filter:activate', _.bind(this.toggleFilter, this));
@@ -117,7 +119,8 @@ issueList.FilterView = Backbone.View.extend({
   },
   initSubViews: function() {
     this.dropdown = new issueList.DropdownView({
-      model: this.model
+      model: this.model,
+      mainView: this.mainView
     });
 
   },
@@ -136,7 +139,6 @@ issueList.FilterView = Backbone.View.extend({
   },
   toggleFilter: function(e) {
     var btn;
-    var filterParam;
     // Stringy e comes from triggered filter:activate event
     if (typeof e === 'string') {
       btn = $('[data-filter=' + e + ']');
@@ -148,30 +150,25 @@ issueList.FilterView = Backbone.View.extend({
     btn.toggleClass('is-active')
        .siblings().removeClass('is-active');
 
-    // Clear the search field
-    issueList.events.trigger('search:clear');
-
     if (btn.hasClass('is-active')) {
-      filterParam = 'stage=' + btn.data('filter');
-      issueList.events.trigger('issues:update', btn.data('filter'));
-      issueList.events.trigger('filter:add-to-model', filterParam, {removeQ: true});
+      this.mainView.params.setParam('stage', btn.data('filter'));
     } else {
-      // Remove existing filters from model and URL
-      issueList.events.trigger('filter:reset-stage', {removeQ: true});
-      issueList.events.trigger('issues:update');
+      this.mainView.params.setParam('stage', '');
     }
   }
 });
 
 issueList.SearchView = Backbone.View.extend({
+  mainView: null,
   el: $('.js-issuelist-search'),
   events: {
-    'click .js-search-button': 'searchIfNotEmpty'
+    'click .js-search-button': 'doSearch'
   },
   keyboardEvents: {
-    'enter': 'searchIfNotEmpty'
+    'enter': 'doSearch'
   },
-  initialize: function() {
+  initialize: function(options) {
+    this.mainView = options.mainView;
     issueList.events.on('search:update', _.bind(this.updateSearchQuery, this));
     issueList.events.on('search:clear', _.bind(this.clearSearchBox, this));
   },
@@ -191,42 +188,53 @@ issueList.SearchView = Backbone.View.extend({
   updateSearchQuery: function(data) {
     this.input.val(data);
   },
-  _isEmpty: true,
-  _currentSearch: null,
-  searchIfNotEmpty: _.debounce(function(e) {
+  doSearch: _.debounce(function(e) {
     var searchValue = e.type === 'click' ? $(e.target).prev().val() :
                       e.target.value;
-    if (searchValue.length) {
-      this._isEmpty = false;
-      // don't search if nothing has changed
-      // (or user just added whitespace)
-      if ($.trim(searchValue) !== this._currentSearch) {
-        this._currentSearch = $.trim(searchValue);
-        this.doSearch(this._currentSearch);
-        // clear any filters that have been set.
-        issueList.events.trigger('filter:clear', {removeQ: false});
-      }
-    }
+    this.mainView.params.setParam('search', searchValue);
+  }, 250),
+});
 
-    // if it's empty and the user searches, show the default results
-    // (but only once)
-    if (!searchValue.length && this._currentSearch !== '') {
-      this._currentSearch = '';
-      this._isEmpty = true;
-      issueList.events.trigger('issues:update');
-    }
-  }, 350),
-  doSearch: _.throttle(function(value) {
-    if (!this._isEmpty) {
-      issueList.events.trigger('issues:update', {query: value});
-    }
-  }, 500)
+/*
+* The applied labels view shows the labels we're filtering by - *excluding*
+* status- prefixed labels. These labels can be un-set by clicking their X
+* or by focusing them by keyboard and pressing del
+*/
+issueList.AppliedLabelsView = Backbone.View.extend({
+  mainView: null,
+  el: $('.wc-AppliedLabels .wc-content'),
+  events: {
+    'click .wc-Filter-appliedlabel': 'deleteLabel'
+  },
+  keyboardEvents: {
+    'delete': 'deleteLabel'
+  },
+  initialize: function(options) {
+    this.mainView = options.mainView;
+    issueList.events.on('appliedlabels:update', _.bind(this.updateAppliedLabels, this));
+
+  },
+  template: _.template($('#issuelist-appliedlabels-tmpl').html()),
+  updateAppliedLabels: function() {
+    var labels = this.mainView.params.get('label');
+    this.render({labels:labels});
+  },
+  render: function(options) {
+    this.$el.html(this.template(options));
+    return this;
+  },
+  deleteLabel: function(e) {
+    var label = e.target.firstChild.textContent.trim();
+    this.mainView.params.deleteLabel(label);
+  }
 });
 
 issueList.SortingView = Backbone.View.extend({
+  mainView: null,
   el: $('.js-issue-sorting'),
   events: {},
-  initialize: function() {
+  initialize: function(options) {
+    this.mainView = options.mainView;
     this.paginationModel = new Backbone.Model({
       dropdownTitle: 'Show 50',
       dropdownOptions: [
@@ -252,10 +260,12 @@ issueList.SortingView = Backbone.View.extend({
   },
   initSubViews: function() {
     this.paginationDropdown = new issueList.DropdownView({
-      model: this.paginationModel
+      model: this.paginationModel,
+      mainView: this.mainView
     });
     this.sortDropdown = new issueList.DropdownView({
-      model: this.sortModel
+      model: this.sortModel,
+      mainView: this.mainView
     });
   },
   template: _.template($('#issuelist-sorting-tmpl').html()),
@@ -271,23 +281,21 @@ var issuesPagination = new PaginationMixin();
 
 issueList.IssueView = Backbone.View.extend(
   _.extend({}, issuesPagination, {
+    mainView: null,
     el: $('.js-issue-list'),
     events: {
       'click .js-issue-label': 'labelSearch',
     },
   // NOTE: these filters don't need "status-" prefixes because appear in URL params
-    _filterRegex: /&*stage=(new|needscontact|needsdiagnosis|contactready|sitewait|closed)&*/i,
-    _searchRegex: /&*q=(?:(.+)?)&*/i,
-    _githubSearchEndpoint: 'https://api.github.com/search/issues',
     _isLoggedIn: $('body').data('username'),
     _loadingIndicator: $('.js-loader'),
     _urlParams: undefined,
-    initialize: function() {
+    initialize: function(options) {
+      this.mainView = options.mainView;
       this.issues = new issueList.IssueCollection();
 
     // set up event listeners.
       issueList.events.on('issues:update', _.bind(this.updateIssues, this));
-      issueList.events.on('filter:add-to-model', _.bind(this.updateModelParams, this));
       issueList.events.on('filter:reset-stage', _.bind(this.resetStageFilter, this));
       wcEvents.on('dropdown:change', _.bind(this.updateModelParams, this));
       window.addEventListener('popstate', _.bind(this.loadIssues, this));
@@ -299,56 +307,20 @@ issueList.IssueView = Backbone.View.extend(
     loadIssues: function() {
     // Attemps to load model state from URL params, if present,
     // otherwise grab model defaults and load issues
-      var category;
-      var queryMatch;
-
       this._urlParams = location.search.slice(1);
-      var urlParams = this._urlParams;
 
     // There are some params in the URL
-      if (urlParams.length !== 0) {
-        queryMatch = urlParams.match(this._searchRegex);
-        if (!this._isLoggedIn && queryMatch) {
-        // We're dealing with an un-authed user, with a q param.
-          this.doGitHubSearch(urlParams);
-          this.updateModelParams(urlParams);
-          _.delay(function() {
-          // TODO: update search input from query param for authed users.
-            issueList.events.trigger('search:update', queryMatch[1]);
-          }, 0);
-        } else if (category = window.location.search.match(this._filterRegex)) {
-        // If there was a stage filter match, fire an event which loads results
-          this.updateModelParams(urlParams);
-          _.delay(function() {
-            issueList.events.trigger('filter:activate', category[1]);
-          }, 0);
-        } else {
-        // Only bother to update/merge model params if we're not loading the defaults
-          if (urlParams !== $.param(this.issues.params)) {
-            this.updateModelParams(urlParams);
-          }
+      if (this._urlParams.length !== 0) {
+        if (this._urlParams !== this.mainView.params.toDisplayURLQuery()) {
+          this.updateModelParams(this._urlParams);
           this.fetchAndRenderIssues();
         }
-      } else {
-      // There are no params in the URL, load the defaults
-        this.updateURLParams();
-        this.fetchAndRenderIssues();
       }
     },
-    doGitHubSearch: function(params) {
-    // Bypass our server and request GitHub search results (from the client)
-    // to avoid being penalized for unauthed Search API requests.
-      var gitHubSearchURL = this._githubSearchEndpoint + '?' +
-                          $.param(this.issues.normalizeAPIParams(params));
-      this.fetchAndRenderIssues({url: gitHubSearchURL});
-    },
-    fetchAndRenderIssues: function(options) {
+    fetchAndRenderIssues: function() {
       var headers = {headers: {'Accept': 'application/json'}};
-      if (options && options.url) {
-        this.issues.url = options.url;
-      } else {
-        this.issues.url = this.issues.path + '?' + $.param(this.issues.params);
-      }
+
+      this.issues.url = this.mainView.params.toBackendURL(this._isLoggedIn);
 
       this._loadingIndicator.addClass('is-active');
       this.issues.fetch(headers).success(_.bind(function() {
@@ -386,143 +358,42 @@ issueList.IssueView = Backbone.View.extend(
       return this;
     },
     labelSearch: function(e) {
-    // clicking on a label in the issues view should trigger a
-    // "search:update" event to populate the view with search results
-    // for the given label.
+    // clicking on a label in the issues view should trigger an update
+    // listing issues for the given label.
       var target = $(e.target);
-      var clickedLabel = target.data('remotename');
-      var labelFilter = 'label:' + clickedLabel;
-      issueList.events.trigger('search:update', labelFilter);
-      issueList.events.trigger('issues:update', {query: labelFilter});
+      this.mainView.params.setParam('label', target.text());
       e.preventDefault();
     },
     resetStageFilter: function(options) {
       this.updateModelParams('page=1&stage=all', options);
     },
-    updateIssues: function(category) {
-    // depending on what category was clicked (or if a search came in),
-    // update the collection instance url property and fetch the issues.
-
-    // new is a special category that must be retrieved via the Search API,
-    // rather than the Issues API (which can return results for labels)
-      var searchAPICategories = ['new'];
-      var issuesAPICategories = ['closed', 'contactready', 'needsdiagnosis',
-                               'needscontact', 'sitewait'];
-      var params = this.issues.params;
-      var paramsCopy;
-    // note: if query is the empty string, it will load all issues from the
-    // '/api/issues' endpoint (which I think we want).
-      if (category && category.query) {
-      // first, add the query to the underlying model, then make a copy of that
-      // which can be manipulated by doGitHubSearch without affecting the params
-      // that are pushed back to the URL bar.
-        paramsCopy = _.cloneDeep($.extend(params, {q: category.query}));
-        if (!this._isLoggedIn) {
-          this.doGitHubSearch(paramsCopy);
-          return;
-        } else {
-          this.issues.setURLState('/api/issues/search', paramsCopy);
-        }
-      } else if (_.contains(searchAPICategories, category)) {
-        this.issues.setURLState('/api/issues/search/' + category, params);
-      } else if (_.contains(issuesAPICategories, category)) {
-        this.issues.setURLState('/api/issues/category/' + category, params);
-      } else {
-        this.issues.setURLState('/api/issues', params);
-      }
-
+    updateIssues: function() {
+    // When this method runs, the code that triggers it must have *already*
+    // set all params appropriately.
       this.fetchAndRenderIssues();
     },
-    addParamsToModel: function(paramsArray) {
-    // this method just puts the params in the model's params property.
-    // paramsArray is an array of param 'key=value' string pairs
-      _.forEach(paramsArray, _.bind(function(param) {
-        var kvArray = param.split('=');
-        var key = kvArray[0];
-        var value = kvArray[1];
-        this.issues.params[key] = value;
-      }, this));
-    },
-    updateModelParams: function(params, options) {
-    // we convert the params string into an array, splitting
-    // on '&' in case of multiple params. those are then
-    // merged into the issues model.
-
-      var hasPerPageChange = params.indexOf('per_page') !== -1;
-      var hasSortChange = params.indexOf('sort') !== -1;
-
-    // call _.uniq() on it to ignore duplicate values
-      var paramsArray = _.uniq(params.split('&'));
-
-      if (options && options.removeQ === true) {
-        delete this.issues.params['q'];
-      }
-
-      this.addParamsToModel(paramsArray);
-
-    //broadcast to each of the dropdowns that they need to update
-      var pageDropdown;
-      if (hasPerPageChange) {
-        pageDropdown = 'per_page=' + this.issues.params.per_page;
-        _.delay(function() {
-          issueList.events.trigger('dropdown:update', pageDropdown);
-        }, 0);
-      }
-
-      var sortDropdown;
-    // all the sort options begin with sort, and end with direction.
-      if (hasSortChange) {
-        sortDropdown = 'sort=' + this.issues.params.sort + '&direction=' + this.issues.params.direction;
-        _.delay(function() {
-          issueList.events.trigger('dropdown:update', sortDropdown);
-        }, 0);
-      }
-
-    // make sure we prevent more than one mutually-exclusive state param
-    // in the model, because that's weird. the "last" param will win.
-      var currentStateParamName;
-      var stateParamsSet = ['state', 'creator', 'mentioned'];
-      var stateParam = _.find(paramsArray, function(paramString) {
-        return _.find(stateParamsSet, function(stateParam) {
-          if (paramString.indexOf(stateParam) === 0) {
-            return currentStateParamName = stateParam;
-          }
-        });
-      });
-
-      if (stateParam !== undefined) {
-      // delete the non-current state params from the stateParamsSet
-        var toDelete = _.without(stateParamsSet, currentStateParamName);
-        _.forEach(toDelete, _.bind(function(param) {
-          delete this.issues.params[param];
-        }, this));
-
-        if (currentStateParamName in this.issues.params) {
-          _.delay(function() {
-            issueList.events.trigger('dropdown:update', stateParam);
-          }, 0);
-        }
-      }
-
-      this.updateURLParams();
-    // only re-request issues if explicitly asked to
-      if (options && options.update === true) {
-        this.fetchAndRenderIssues();
-      }
+    updateModelParams: function(paramsStr) {
+    // Params from a query string are merged into the issues model.
+      this.mainView.params.fromQueryString(paramsStr);
     },
     updateURLParams: function() {
     // push params from the model back to the URL so it can be used for bookmarks,
     // link sharing, etc.
       var urlParams = this._urlParams;
-      var serializedModelParams = $.param(this.issues.params);
+      var serializedModelParams = this.mainView.params.toDisplayURLQuery();
 
     // only do this if there's something to change
       if (urlParams !== serializedModelParams) {
         this._urlParams = serializedModelParams;
-        if (history.pushState) {
-          history.pushState({}, '', '?' + serializedModelParams);
+        if (history.pushState && history.replaceState) {
+          if (location.href === '/issues') {
+            history.replaceState({}, '', '?' + serializedModelParams);
+          } else {
+            history.pushState({}, '', '?' + serializedModelParams);
+          }
         }
       }
+      issueList.events.on('url:update', _.bind(this.updateURLParams, this));
     }
   }));
 
@@ -533,23 +404,25 @@ issueList.MainView = Backbone.View.extend({
     'g': 'githubWarp',
     'G': 'githubWarp'
   },
-  initialize: function() {
-    this.initSubViews();
+  initialize: function(options) {
+    this.params = new issueList.QueryParams(options && options.params);
+    this.initSubViews(this);
   },
   githubWarp: function(e) {
     // make sure we're not typing in the search input.
     if (e.target.nodeName === 'INPUT') {
       return;
     }
-
+    e.preventDefault(); // some browsers start inline find
     var warpPipe = 'https://github.com/' + repoPath;
     return location.href = warpPipe;
   },
-  initSubViews: function() {
-    this.issueList = new issueList.IssueView();
-    this.issueSorter = new issueList.SortingView();
-    this.filter = new issueList.FilterView();
-    this.search = new issueList.SearchView();
+  initSubViews: function(mainView) {
+    this.issueList = new issueList.IssueView({mainView:mainView});
+    this.issueSorter = new issueList.SortingView({mainView:mainView});
+    this.filter = new issueList.FilterView({mainView:mainView});
+    this.search = new issueList.SearchView({mainView:mainView});
+    this.appliedLabels = new issueList.AppliedLabelsView({mainView:mainView});
 
     this.render();
   },
