6da27cadabba4948e7d0703f60a4b4f628383474
Mike Taylor <miket@mozilla.com>
2015-12-21 14:27:51 -0600

    Issue #882. Import TestingFileStorage from Flask-Uploads.

--- tests/test_uploads.py
+++ tests/test_uploads.py
@@ -11,7 +11,6 @@ import sys
 import unittest
 
 from flask import Request
-from flaskext.uploads import TestingFileStorage
 from StringIO import StringIO
 from werkzeug import FileStorage
 from werkzeug.datastructures import MultiDict
@@ -20,6 +19,47 @@ from werkzeug.datastructures import MultiDict
 sys.path.append(os.path.realpath(os.pardir))
 
 from webcompat import app
+
+
+# taken from https://github.com/srusskih/flask-uploads/blob/master/flaskext/uploads.py#L476 # nopep8
+class TestingFileStorage(FileStorage):
+    """
+    This is a helper for testing upload behavior in your application. You
+    can manually create it, and its save method is overloaded to set `saved`
+    to the name of the file it was saved to. All of these parameters are
+    optional, so only bother setting the ones relevant to your application.
+
+    :param stream: A stream. The default is an empty stream.
+    :param filename: The filename uploaded from the client. The default is the
+                     stream's name.
+    :param name: The name of the form field it was loaded from. The default is
+                 `None`.
+    :param content_type: The content type it was uploaded as. The default is
+                         ``application/octet-stream``.
+    :param content_length: How long it is. The default is -1.
+    :param headers: Multipart headers as a `werkzeug.Headers`. The default is
+                    `None`.
+    """
+    def __init__(self, stream=None, filename=None, name=None,
+                 content_type='application/octet-stream', content_length=-1,
+                 headers=None):
+        FileStorage.__init__(self, stream, filename, name=name,
+                             content_type=content_type,
+                             content_length=content_length, headers=None)
+        self.saved = None
+
+    def save(self, dst, buffer_size=16384):
+        """
+        This marks the file as saved by setting the `saved` attribute to the
+        name of the file it was saved to.
+
+        :param dst: The file to save to.
+        :param buffer_size: Ignored.
+        """
+        if isinstance(dst, basestring):
+            self.saved = dst
+        else:
+            self.saved = dst.name
 
 
 class TestUploads(unittest.TestCase):
