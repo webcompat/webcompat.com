196f87baa1b0e436a36e2f96a1f75f6c6878efa3
Mike Taylor <miket@mozilla.com>
2015-12-29 17:11:50 -0600

    Issue #786. Create a config module to split up config.py
    As a result, we no longer need config.py(.example)

---
+++ config/__init__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+'''This module handles application configuration and secrets.'''
+
+import os
+from datetime import datetime
+from collections import namedtuple
+
+from env_constants import *
+from secrets import *
+
+THREADS_PER_PAGE = 8
+
+# ~3 months-ish expires for static junk
+SEND_FILE_MAX_AGE_DEFAULT = 9000000
+
+# Flask Limiter settings
+# See http://flask-limiter.readthedocs.org/en/latest/#configuration
+RATELIMIT_STORAGE_URL = 'memory://'
+RATELIMIT_STRATEGY = 'moving-window'
+
+DEBUG = False
+
+if not PRODUCTION:
+    DEBUG = True
+
+# Logging Capabilities
+# To benefit from the logging, you may want to add:
+#   app.logger.info(Thing_To_Log)
+# it will create a line with the following format
+# 2015-09-14 20:50:19,185 INFO: Thing_To_Log [in /codepath/views.py:127]
+LOG_FILE = '/tmp/webcompat.log'
+LOG_FMT = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+
+# Status categories used in the project
+# 'new', 'needsdiagnosis', 'needscontact', 'contactready' , 'sitewait', 'close'
+# Creating the model
+Category = namedtuple('Category', ['name', 'dataAttribute', 'label'])
+CATEGORIES = []
+cat_labels = [('new', 'new', 'New Issues'),
+              ('needsDiagnosis', 'needsdiagnosis', 'Needs Diagnosis'),
+              ('needsContact', 'needscontact', 'Needs Contact'),
+              ('ready', 'contactready', 'Ready for Outreach'),
+              ('sitewait', 'sitewait', 'Site Contacted'),
+              ('close', 'closed', 'Closed')]
+# populating the categories for issues status
+for cat_label in cat_labels:
+    CATEGORIES.append(Category(name=cat_label[0],
+                               dataAttribute=cat_label[1],
+                               label=cat_label[2]))
---
+++ config/env_constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+# Define the application directory
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+# the PRODUCTION and DEVELOPMENT environment variables are set in uwsgi.conf
+# on the webcompat server. If they're not set, they will default to None
+PRODUCTION = os.environ.get('PRODUCTION')
+DEVELOPMENT = os.environ.get('DEVELOPMENT')
+# Are we serving the app from localhost?
+LOCALHOST = not PRODUCTION and not DEVELOPMENT
