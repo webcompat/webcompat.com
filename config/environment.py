8dd93ce9177dd7fb5e882693057a2afa275f81ab
Mike Taylor <miket@mozilla.com>
2015-12-30 10:57:49 -0600

    Issue #786. s/DEVELOPMENT/STAGING/.

--- config/environment.py
+++ config/environment.py
@@ -12,6 +12,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 # the PRODUCTION and DEVELOPMENT environment variables are set in uwsgi.conf
 # on the webcompat server. If they're not set, they will default to None
 PRODUCTION = os.environ.get('PRODUCTION')
-DEVELOPMENT = os.environ.get('DEVELOPMENT')
+# locally, we refer to this as STAGING
+STAGING = os.environ.get('DEVELOPMENT')
 # Are we serving the app from localhost?
-LOCALHOST = not PRODUCTION and not DEVELOPMENT
+LOCALHOST = not PRODUCTION and not STAGING
