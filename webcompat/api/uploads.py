2c83ead644d706aadfd36d5a38e7b34ab13facea
Mike Taylor <miket@mozilla.com>
2016-05-26 12:35:14 -0500

    Issue #1063. Re-allow image uploads from form elements for issue uploads.

--- webcompat/api/uploads.py
+++ webcompat/api/uploads.py
@@ -17,6 +17,7 @@ from flask import Blueprint
 from flask import request
 from io import BytesIO
 from PIL import Image
+from werkzeug.datastructures import FileStorage
 from werkzeug.exceptions import RequestEntityTooLarge
 from uuid import uuid4
 
@@ -43,7 +44,10 @@ class Upload(object):
     def to_image_object(self, imagedata):
         '''Method to return a Pillow Image object from the raw imagedata.'''
         try:
-            # Make sure we're being sent a base64 encoded image
+            # Is this a file upload (i.e., issue form upload)?
+            if isinstance(imagedata, FileStorage):
+                return Image.open(imagedata)
+            # Is this a base64 encoded image (i.e., bug report screenshot)?
             if (isinstance(imagedata, unicode) and
                     imagedata.startswith('data:image/')):
                 # Chop off 'data:image/.+;base64,' before decoding
@@ -106,7 +110,9 @@ def upload():
 
     Returns a JSON string that contains the filename and url.
     '''
-    if 'image' in request.form:
+    if 'image' in request.files and request.files['image'].filename:
+        imagedata = request.files['image']
+    elif 'image' in request.form:
         imagedata = request.form['image']
     else:
         # We don't know what you're trying to do, but it ain't gonna work.
