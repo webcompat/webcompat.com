555c60bf5c43064f0c93d8ce22eaeac241da5416
Karl Dubost <karl+github@la-grange.net>
2016-10-07 13:52:17 +0900

    #722 Simplifies the image path computation

--- webcompat/api/uploads.py
+++ webcompat/api/uploads.py
@@ -40,7 +40,16 @@ class Upload(object):
 
     def __init__(self, imagedata):
         self.image_object = self.to_image_object(imagedata)
+        # computing the parameters to be used
+        today = datetime.date.today()
+        self.year = str(today.year)
+        self.month = str(today.month)
+        self.image_id = str(uuid.uuid4())
         self.file_ext = self.get_file_ext()
+        self.image_path = self.img_path(self.month, self.year, self.image_id,
+                                        thumb=False)
+        self.thumb_path = self.img_path(self.month, self.year, self.image_id,
+                                        thumb=True)
 
     def to_image_object(self, imagedata):
         '''Method to return a Pillow Image object from the raw imagedata.'''
@@ -59,6 +68,14 @@ class Upload(object):
             # Not a valid format
             abort(415)
 
+    def img_path(self, month, year, image_id, thumb=False):
+        '''Return the right image path.'''
+        thumb_string = ''
+        if thumb:
+            thumb_string = '-thumb'
+        image_name = image_id + thumb_string + '.' + self.file_ext
+        return os.path.join(year, month, image_name)
+
     def get_file_ext(self):
         '''Method to return the file extension, as determined by Pillow.
 
@@ -74,23 +91,15 @@ class Upload(object):
 
     def get_url(self):
         '''Method to return a URL for the uploaded file.'''
-        return app.config['UPLOADS_DEFAULT_URL'] + self.file_path
+        return app.config['UPLOADS_DEFAULT_URL'] + self.image_path
 
     def save(self):
         '''Check that the file is allowed, then save to filesystem.'''
         save_parameters = {}
         if self.file_ext not in self.ALLOWED_FORMATS:
             raise TypeError('Image file format not allowed')
-
-        today = datetime.date.today()
-        year = str(today.year)
-        month = str(today.month)
-        image_id = str(uuid.uuid4())
-        self.file_path = os.path.join(year, month,
-                                      image_id + '.' + self.file_ext)
-        self.thumb_path = os.path.join(year, month,
-                                       image_id + 'thumb.' + self.file_ext)
-        file_dest = app.config['UPLOADS_DEFAULT_DEST'] + self.file_path
+        # Paths of the images
+        file_dest = app.config['UPLOADS_DEFAULT_DEST'] + self.image_path
         thumb_dest = app.config['UPLOADS_DEFAULT_DEST'] + self.thumb_path
         dest_dir = os.path.dirname(file_dest)
         if not os.path.exists(dest_dir):
