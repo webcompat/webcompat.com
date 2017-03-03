db4e879a167604debfa04d3e208be68cb4f64c6d
Mike Taylor <miket@mozilla.com>
2017-03-03 15:00:46 -0600

    Revert "Issue #609 - Implement Cache-Policy decorator"
    This reverts commit bab6f6df1676b65f891ace94527d4596f176dcbf.

--- webcompat/api/endpoints.py
+++ webcompat/api/endpoints.py
@@ -9,6 +9,7 @@
 This is used to make API calls to GitHub, either via a logged-in users
 credentials or as a proxy on behalf of anonymous or unauthenticated users.'''
 
+import json
 
 from flask import abort
 from flask import Blueprint
