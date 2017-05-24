5f5b6e93da74d7bd5ab2f1660e168cf2af49b1ca
Karl Dubost <karl+github@la-grange.net>
2017-05-24 10:27:29 +0900

    Issue #1228 - Removes unused header

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -18,11 +18,7 @@ from webcompat.webhooks import helpers
 sys.path.append(os.path.realpath(os.pardir))
 import webcompat  # nopep8
 
-
-# Any request that depends on parsing HTTP Headers (basically anything
-# on the index route, will need to include the following: environ_base=headers
-headers = {'HTTP_USER_AGENT': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; '
-                               'rv:31.0) Gecko/20100101 Firefox/31.0')}
+# The key is being used for testing and computing the signature.
 key = app.config['HOOK_SECRET_KEY']
 
 
