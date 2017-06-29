5c547e53e1256ab60dccb3c13a55709a19583c43
Beatriz Rizental Machado <beatriz.rizental@gmail.com>
2017-06-29 14:59:03 -0700

    Fixes #1620 - Gets all functional tests to work offline.

--- tests/functional/issue-list-non-auth.js
+++ tests/functional/issue-list-non-auth.js
@@ -197,14 +197,15 @@ define(
           .end()
           .sleep(500)
           .getCurrentUrl()
-          .then(function(url) {
-            assert.match(
-              url,
-              /[https://github.com/^*/^*/issues/]/,
-              "We're at GitHub now."
-            );
-          })
-          .end();
+          .then(
+            function(url) {
+              assert.match(
+                url,
+                /[https://github.com/^*/^*/issues/]/,
+                "We're at GitHub now."
+              );
+            }.end()
+          );
       },
 
       "Pressing g inside of search input *doesn't* go to github issues": function() {
