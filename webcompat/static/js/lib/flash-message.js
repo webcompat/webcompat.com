697d8b5613bfe3c9ab3a2f17604dc07a9a77cc88
Mike Taylor <miket@mozilla.com>
2017-08-01 11:06:37 -0500

    Issue #1679. Add external .jst template for showThanks flash message.

--- webcompat/static/js/lib/flash-message.js
+++ webcompat/static/js/lib/flash-message.js
@@ -44,16 +44,7 @@ var FlashMessageView = Backbone.View.extend({
     this.render(message);
   },
   showThanks: function(opts) {
-    var buildTemplate = _.template(
-      [
-        "<h4>Thanks for reporting an issue!</h4>",
-        "<p>You're helping us make the web a better place to work and play.</p>",
-        "<p>Tell your friends about the bug you just filed:</p>",
-        '<a class="wc-Button wc-Button--action" href="https://twitter.com/intent/tweet?text=<%- encodeURIComponent("I just filed a bug on the internet:") %>&url=<%- encodeURIComponent("https://webcompat.com/issues/") %><%= number %>&via=webcompat" target="_blank">Share on Twitter</a>',
-        '<a class="wc-Button wc-Button--action" href="https://facebook.com/sharer/sharer.php?u=<%- encodeURIComponent("https://webcompat.com/issues/") %><%= number %>" target="_blank">Share on Facebook</a>'
-      ].join("")
-    );
-
+    var buildTemplate = wcTmpl["issue/thanks.jst"];
     this.$el.addClass("is-active wc-FlashMessage--thanks");
     this.$el
       .html(buildTemplate({ number: opts.message }))
