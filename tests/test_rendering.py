a54c1e7a6e34b359b0b5fdbec4fa7adef91b4706
Karl Dubost <karl+github@la-grange.net>
2017-08-25 14:57:31 +0900

    Issue #1428 - Adds test for activity page

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -20,8 +20,8 @@ headers = {'HTTP_USER_AGENT': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; '
                                'rv:31.0) Gecko/20100101 Firefox/31.0')}
 
 
-class TestURLs(unittest.TestCase):
-    """Tests for the content once HTML remplates are rendered."""
+class TestURIContent(unittest.TestCase):
+    """Tests for the content once HTML templates are rendered."""
 
     def setUp(self):
         """Set up the tests."""
@@ -31,6 +31,13 @@ class TestURLs(unittest.TestCase):
     def tearDown(self):
         """Tear down the tests."""
         pass
+
+    def login(self, username, password):
+        """Create a call to the login page to initiate g."""
+        return self.app.get('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
 
     def test_titles(self):
         """Page title format for different URIs."""
@@ -52,6 +59,14 @@ class TestURLs(unittest.TestCase):
             expected = '<title>{title} | webcompat.com</title>'.format(
                 title=title)
             self.assertTrue(expected in rv.data)
+
+    def test_user_title_pages(self):
+        """Testing user activity page title."""
+        title = "<title>testuser's Activity | webcompat.com</title>"
+        with webcompat.app.app_context():
+            self.login('testuser', 'foobar')
+            rv = self.app.get('/activity/testuser')
+            self.assertTrue(title in rv.data)
 
 if __name__ == '__main__':
     unittest.main()
