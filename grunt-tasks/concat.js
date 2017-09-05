5516bc382c2e921a70eb44a78ee5ed032f713ee7
Mike Taylor <miketaylr@gmail.com>
2017-09-05 14:30:22 +0800

    Issue #1745. Part 1 - Don't include labels.js where its not needed.

--- grunt-tasks/concat.js
+++ grunt-tasks/concat.js
@@ -30,7 +30,6 @@ module.exports = function(grunt) {
     diagnose: {
       src: [
         "<%= jsPath %>/lib/models/label-list.js",
-        "<%= jsPath %>/lib/labels.js",
         "<%= jsPath %>/lib/models/issue.js",
         "<%= jsPath %>/lib/diagnose.js"
       ],
@@ -50,7 +49,6 @@ module.exports = function(grunt) {
     issueList: {
       src: [
         "<%= jsPath %>/lib/models/label-list.js",
-        "<%= jsPath %>/lib/labels.js",
         "<%= jsPath %>/lib/models/issue.js",
         "<%= jsPath %>/lib/mixins/pagination.js",
         "<%= jsPath %>/lib/issue-list.js"
