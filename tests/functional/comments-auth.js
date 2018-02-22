e30c42ee95854b98746ccdc980f077fd72fccd63
Mike Taylor <miketaylr@gmail.com>
2018-02-22 10:06:44 -0500

    dummy commit to trigger build

--- tests/functional/comments-auth.js
+++ tests/functional/comments-auth.js
@@ -1,7 +1,9 @@
-"use strict";
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
 const intern = require("intern").default;
 const { assert } = intern.getPlugin("chai");
 const { registerSuite } = intern.getInterface("object");
