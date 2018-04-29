0fb685fe3dd2cc2d532db28f6e08f523f15ef506
Karl Dubost <karl+github@la-grange.net>
2018-04-30 08:17:16 +0900

    Issue #2431 - Adds tests for changelog tool.

---
+++ tests/unit/test_tools_changelog.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Tests for the changelog generator."""
+
+import unittest
+
+from tools import changelog
+
+
+class TestChangelog(unittest.TestCase):
+    """Test for Changelog formatter."""
+
+    def setUp(self):
+        """Set up the tests."""
+        pass
+
+    def tearDown(self):
+        """Tear down the tests."""
+        pass
+
+    def test_create_changelog(self):
+        """Final changelog formatting."""
+        changes = [
+            {'title': 'Fixes #1 - Everything you can imagine is real.',
+             'number': '2',
+             'html_url': 'https://github.com/webcompat/webcompat.com/pull/2'},
+        ]
+        expected = '* Fixes #1 - Everything you can imagine is real. [Pull #2](https://github.com/webcompat/webcompat.com/pull/2)\n'  # noqa
+        actual = changelog.create_changelog(changes)
+        self.assertEqual(actual, expected)
+
+    def test_normalize_title(self):
+        """Normalize the title."""
+        # Issue instead of Fixes
+        actual = changelog.normalize_title(
+            'Issue #101 - Everything you can imagine is real.')
+        expected = 'Fixes #101 - Everything you can imagine is real.'
+        self.assertEqual(actual, expected)
+        # Trailing dot
+        actual = changelog.normalize_title(
+            'Fixes #101. Everything you can imagine is real.')
+        expected = 'Fixes #101 - Everything you can imagine is real.'
+        self.assertEqual(actual, expected)
+        # Space lacking after the number
+        actual = changelog.normalize_title(
+            'Fixes #101- Everything you can imagine is real.')
+        expected = 'Fixes #101 - Everything you can imagine is real.'
+        self.assertEqual(actual, expected)
+        # Wrong separator.
+        actual = changelog.normalize_title(
+            'Fixes #101 — Everything you can imagine is real.')
+        expected = 'Fixes #101 - Everything you can imagine is real.'
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
