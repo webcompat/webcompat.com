fc98bebd86d4db7dbc8e3f58e9aaf91d38ed07f1
Christian Stuff <christian.stuff@namics.com>
2018-06-21 19:48:17 +0200

    Issue #2506 - Removed unnecessary eslint-disable comment

--- webcompat/static/js/lib/autogrow-textfield.js
+++ webcompat/static/js/lib/autogrow-textfield.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/*eslint new-cap: ["error", { "capIsNewExceptions": ["Deferred"] }]*/
-
 function Autogrow() {
   this.autogrowField = $(".js-autogrow-field");
 
