f15d07517aad335b3ac233e0d54b1f9612c632e1
Mike Taylor <miketaylr@gmail.com>
2018-07-05 19:41:11 -0500

    Issue #2507 - Add a needsinfo section to activity page

--- webcompat/static/js/lib/user-activity.js
+++ webcompat/static/js/lib/user-activity.js
@@ -9,6 +9,7 @@ issueList.user = $("body").data("username");
 
 var myIssuesPagination = new PaginationMixin();
 var mentionsPagination = new PaginationMixin();
+var needsinfoPagination = new PaginationMixin();
 
 // UserActivityCollection inherits from IssueCollection, which doesn't set
 // its url property directly. So we need to be sure to construct that from
@@ -19,6 +20,72 @@ issueList.UserActivityCollection = issueList.IssueCollection.extend({
       "/api/issues/" + issueList.user + options.path + "?" + options.params;
   }
 });
+
+issueList.NeedsInfoView = Backbone.View.extend(
+  _.extend({}, needsinfoPagination, {
+    el: $("#needsinfo-issues"),
+    _loadingIndicator: loadingIndicator,
+    initialize: function() {
+      this.issues = new issueList.UserActivityCollection({
+        path: "/needsinfo",
+        params: "per_page=10"
+      });
+      needsinfoPagination.initMixin(this, this.issues, $("#needsinfo-section"));
+      this.fetchAndRenderIssues({ url: this.issues.url });
+    },
+    template: wcTmpl["web_modules/issue-list.jst"],
+    render: function() {
+      this.$el.html(
+        this.template({
+          issues: this.issues.toJSON()
+        })
+      );
+
+      return this;
+    },
+    updateModelParams: function() {
+      //no-op for now, (if?) until we manage state in the URL
+    },
+    fetchAndRenderIssues: function(options) {
+      var headers = { headers: { Accept: "application/json" } };
+      if (options && options.url) {
+        this.issues.url = options.url;
+      } else {
+        this.issues.url = this.issues.path + "?" + $.param(this.issues.params);
+      }
+
+      this._loadingIndicator.addClass("is-active");
+      this.issues
+        .fetch(headers)
+        .done(
+          _.bind(function() {
+            this._loadingIndicator.removeClass("is-active");
+            this.render(this.issues);
+            myIssuesPagination.initPaginationLinks(this.issues);
+          }, this)
+        )
+        .fail(
+          _.bind(function(e) {
+            var message;
+            var timeout;
+            if (e.responseJSON) {
+              message = e.responseJSON.message;
+              timeout = e.responseJSON.timeout * 1000;
+            } else {
+              message = "Something went wrong!";
+              timeout = 4000;
+            }
+
+            this._loadingIndicator.removeClass("is-active");
+            wcEvents.trigger("flash:error", {
+              message: message,
+              timeout: timeout
+            });
+          }, this)
+        );
+    }
+  })
+);
 
 issueList.MyIssuesView = Backbone.View.extend(
   _.extend({}, myIssuesPagination, {
@@ -161,6 +228,7 @@ issueList.IssueMentionsView = Backbone.View.extend(
 );
 
 $(function() {
+  new issueList.NeedsInfoView();
   new issueList.MyIssuesView();
   new issueList.IssueMentionsView();
 });
