0c3468a78da0865b8509575e27caaae8050aa529
Mariana Meireles <mariana@psychonautgirl.space>
2019-01-10 17:39:08 -0200

    Fixes #2757 - Add markdown-it to issues on production

--- grunt-tasks/concat.js
+++ grunt-tasks/concat.js
@@ -35,6 +35,9 @@ module.exports = function(grunt) {
     },
     issues: {
       src: [
+        "<%= jsPath %>/vendor/markdown-it.js",
+        "<%= jsPath %>/vendor/markdown-it-emoji-1.4.0.js",
+        "<%= jsPath %>/vendor/markdown-it-sanitizer-0.4.1.js",
         "<%= jsPath %>/lib/models/label-list.js",
         "<%= jsPath %>/lib/editor.js",
         "<%= jsPath %>/lib/labels.js",
