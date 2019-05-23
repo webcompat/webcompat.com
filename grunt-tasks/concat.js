5bd3ed16d52e17567ffcf182b5ea5b7fe80dd2d6
Karl Dubost <karl+github@la-grange.net>
2019-05-23 14:04:14 +0900

    Revert "Fixes #2873 - Add AB testing plumbing"

--- grunt-tasks/concat.js
+++ grunt-tasks/concat.js
@@ -69,15 +69,6 @@ module.exports = function(grunt) {
         "<%= jsPath %>/lib/user-activity.js"
       ],
       dest: "<%= jsDistPath %>/user-activity.js"
-    },
-    ab: {
-      src: [
-        "<%= jsDistPath %>/mozilla-cookie-helper.js",
-        "<%= jsDistPath %>/mozilla-dnt-helper.js",
-        "<%= jsDistPath %>/mozilla-traffic-cop.js",
-        "<%= jsPath %>/lib/ab.js"
-      ],
-      dest: "<%= jsDistPath %>/ab.js"
     }
   });
 };
