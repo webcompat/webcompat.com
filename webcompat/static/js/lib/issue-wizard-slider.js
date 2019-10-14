2ec6f07509b92c81143133817dee2ec5f07f2c17
Roman Munteanu <munteanu.int@gmail.com>
2019-10-14 18:40:16 +0200

    Fixes #2977 - Pop-ups with slider on mobile

--- webcompat/static/js/lib/issue-wizard-slider.js
+++ webcompat/static/js/lib/issue-wizard-slider.js
@@ -12,6 +12,7 @@ function Slider() {
   };
 
   this.sliderHandler = function() {
+    var slider = document.querySelectorAll(".slider");
     var nextBtn = document.querySelectorAll(".slider .next");
     var prevBtn = document.querySelectorAll(".slider .prev");
     var dot = document.querySelectorAll(".slider .dot");
@@ -20,6 +21,8 @@ function Slider() {
     var controlFinish = document.querySelector(
       ".slider .controls .close-control"
     );
+    var touchstartX = 0;
+    var touchendX = 0;
     // Intercepted popup close buttons that trigger the return to first slide
     var closeButtons = document.querySelectorAll(".popup-modal__close");
     var index = 0;
@@ -41,29 +44,37 @@ function Slider() {
       }
     };
 
+    var nextSlide = function() {
+      index++;
+      if (index >= slide.length) {
+        index--;
+        return false;
+      }
+      slide[index - 1].classList.remove("active");
+      slide[index].classList.add("active");
+      setActiveControls(index);
+    };
+
+    var prevSlide = function() {
+      index--;
+      if (index < 0) {
+        index = 0;
+        return false;
+      }
+      slide[index + 1].classList.remove("active");
+      slide[index].classList.add("active");
+      setActiveControls(index);
+    };
+
     prevBtn.forEach(function(btn) {
       btn.addEventListener("click", function() {
-        index--;
-        if (index < 0) {
-          index = 0;
-          return false;
-        }
-        slide[index + 1].classList.remove("active");
-        slide[index].classList.add("active");
-        setActiveControls(index);
+        prevSlide();
       });
     });
 
     nextBtn.forEach(function(btn) {
       btn.addEventListener("click", function() {
-        index++;
-        if (index >= slide.length) {
-          index--;
-          return false;
-        }
-        slide[index - 1].classList.remove("active");
-        slide[index].classList.add("active");
-        setActiveControls(index);
+        nextSlide();
       });
     });
 
@@ -83,6 +94,22 @@ function Slider() {
         slide[index].classList.add("active");
         dot[index].click();
         setActiveControls(index);
+      });
+    });
+
+    function handleGesure() {
+      if (touchendX < touchstartX) nextSlide();
+      if (touchendX > touchstartX) prevSlide();
+    }
+
+    slider.forEach(function(s) {
+      s.addEventListener("touchstart", e => {
+        touchstartX = e.changedTouches[0].screenX;
+      });
+
+      s.addEventListener("touchend", e => {
+        touchendX = e.changedTouches[0].screenX;
+        handleGesure();
       });
     });
   };
