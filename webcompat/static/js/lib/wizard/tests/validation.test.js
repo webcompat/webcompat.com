8156ba061942f5d04f5bc7a0d52a929cda116bbe
Ksenia Berezina <kberezina@mozilla.com>
2020-03-30 14:12:36 -0400

    Fixes #3232 - Add validation for step 2 when selecting "Something else"

--- webcompat/static/js/lib/wizard/tests/validation.test.js
+++ webcompat/static/js/lib/wizard/tests/validation.test.js
@@ -1,4 +1,4 @@
-import { isUrlValid } from "../validation.js";
+import { isUrlValid, isEmpty } from "../validation.js";
 
 const { describe, it } = intern.getPlugin("interface.bdd");
 const { assert } = intern.getPlugin("chai");
@@ -12,5 +12,11 @@ describe("validation", () => {
     assert.isFalse(isUrlValid("www.example.com"));
     assert.isFalse(isUrlValid("https://"));
     assert.isFalse(isUrlValid(" "));
+  });
+
+  it("isEmpty detects if string is empty trimming spaces", () => {
+    assert.isTrue(isEmpty(""));
+    assert.isTrue(isEmpty(" "));
+    assert.isFalse(isEmpty("test"));
   });
 });
