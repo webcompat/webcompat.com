e62d9e7fb48d194145b03b646ca5deb5732bc880
Mike Taylor <miket@mozilla.com>
2020-04-08 16:04:40 -0500

    Issue #2198 - Pass in targetOrigin from intern config for tests
    (Rather than hard-code)

--- tests/functional/image-uploads-non-auth.js
+++ tests/functional/image-uploads-non-auth.js
@@ -8,6 +8,10 @@ const { assert } = intern.getPlugin("chai");
 const { registerSuite } = intern.getInterface("object");
 const FunctionalHelpers = require("./lib/helpers.js");
 const url = intern.config.functionalBaseUrl + "issues/new";
+const targetOrigin = intern.config.functionalBaseUrl.slice(
+  0,
+  intern.config.functionalBaseUrl.length - 1
+);
 
 // This string is executed by calls to `execute()` in various tests
 // it postMessages a small green test square.
@@ -39,9 +43,12 @@ registerSuite("Image Uploads (non-auth)", {
         FunctionalHelpers.openPage(this, url, ".js-image-upload")
           // Build up a green test square in canvas, toBlob that, and then postMessage the blob
           // see window-helpers.js for more details.
-          .execute(function () {
-            WindowHelpers.getBlob().then(WindowHelpers.sendBlob);
-          })
+          .execute(
+            function (args) {
+              WindowHelpers.getBlob(args).then(WindowHelpers.sendBlob);
+            },
+            [targetOrigin]
+          )
           .sleep(1000)
           .findByCssSelector(".js-image-upload")
           .getAttribute("style")
