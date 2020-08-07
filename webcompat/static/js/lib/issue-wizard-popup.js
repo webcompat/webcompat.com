034b3687001abdb3cfd46af9eaeee70aa57a9c80
Mike Taylor <miket@mozilla.com>
2020-08-07 17:51:40 -0500

    Issue #3391 - Part 1. Factor out popupHandler into an open and close method

--- webcompat/static/js/lib/issue-wizard-popup.js
+++ webcompat/static/js/lib/issue-wizard-popup.js
@@ -7,35 +7,49 @@ import { sendAnalyticsEvent } from "./wizard/analytics.js";
 
 function Popup() {
   this.init = function () {
+    this.closeButtons = document.querySelectorAll(".popup-modal__close");
+    this.modalTriggers = document.querySelectorAll(".popup-trigger");
+    this.overlay = document.querySelector(".popup-overlay");
+
     this.setUpEvents();
   };
 
   this.setUpEvents = function () {
-    this.popupHandler();
+    this.modalTriggers.forEach(
+      function (trigger) {
+        trigger.addEventListener("click", this.openModal.bind(this));
+      }.bind(this)
+    );
+
+    this.closeButtons.forEach(
+      function (btn) {
+        btn.addEventListener("click", this.closeModal.bind(this));
+      }.bind(this)
+    );
   };
 
-  this.popupHandler = function () {
-    var modalTriggers = document.querySelectorAll(".popup-trigger");
-    var overlay = document.querySelector(".popup-overlay");
-    var closeButtons = document.querySelectorAll(".popup-modal__close");
-    modalTriggers.forEach(function (trigger) {
-      trigger.addEventListener("click", function (e) {
-        e.preventDefault();
-        sendAnalyticsEvent("whatIsCompat", "click");
-        var popupTrigger = trigger.dataset.popupTrigger;
-        var popupModal = document.querySelector(
-          '[data-popup-modal="'.concat(popupTrigger, '"]')
-        );
-        popupModal.classList.add("is--visible");
-        overlay.classList.add("is-blacked-out");
-        closeButtons.forEach(function (btn) {
-          btn.addEventListener("click", function () {
-            popupModal.classList.remove("is--visible");
-            overlay.classList.remove("is-blacked-out");
-          });
-        });
-      });
-    });
+  this.openModal = function (e) {
+    e.preventDefault();
+
+    var popupTrigger = e.target.dataset.popupTrigger;
+    var popupModal = document.querySelector(
+      '[data-popup-modal="'.concat(popupTrigger, '"]')
+    );
+    sendAnalyticsEvent(
+      // transform trigger into camelCase and send as our event
+      popupTrigger.replace(/-([a-z])/g, function (match) {
+        return match[1].toUpperCase();
+      }),
+      "click"
+    );
+    popupModal.classList.add("is--visible");
+    this.overlay.classList.add("is-blacked-out");
+  };
+
+  this.closeModal = function () {
+    var popupModal = document.querySelector(".popup-modal.is--visible");
+    popupModal.classList.remove("is--visible");
+    this.overlay.classList.remove("is-blacked-out");
   };
 
   return this.init();
